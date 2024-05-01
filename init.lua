dc4544bf6a38b1dd478929769d02ae1f9d3c5215
xunuwu <xunuwu@gmail.com>
2024-05-01 17:59:26 +0200

    conform.nvim

--- init.lua
+++ init.lua
@@ -1,144 +1,166 @@
-pcall(function() vim.loader.enable() end)
+pcall(function()
+	vim.loader.enable()
+end)
 
 -- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+local path_package = vim.fn.stdpath("data") .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
-   vim.cmd('echo "Installing `mini.nvim`" | redraw')
-   local clone_cmd = {
-      'git', 'clone', '--filter=blob:none',
-      'https://github.com/echasnovski/mini.nvim', mini_path
-   }
-   vim.fn.system(clone_cmd)
-   vim.cmd('packadd mini.nvim | helptags ALL')
-   vim.cmd('echo "Installed `mini.nvim`" | redraw')
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/echasnovski/mini.nvim",
+		mini_path,
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd("packadd mini.nvim | helptags ALL")
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
-require('mini.deps').setup({ path = { package = path_package } })
+require("mini.deps").setup({ path = { package = path_package } })
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
-now(function() require('opts') end)
-now(function() require('keys') end)
+now(function()
+	require("opts")
+end)
+now(function()
+	require("keys")
+end)
 
 now(function()
-   require('mini.statusline').setup()
+	require("mini.statusline").setup()
 end)
 
 later(function()
-   require("mini.extra").setup()
+	require("mini.extra").setup()
 end)
-
-
-
-
-
 
 require("mini.pairs").setup()
 
 later(function()
-   require("mini.pick").setup()
-   vim.keymap.set("n", "<leader>f", MiniPick.builtin.files)
-   vim.keymap.set("n", "<leader>ss", MiniPick.builtin.grep_live)
-   vim.keymap.set("n", "<leader>sg", MiniPick.builtin.grep)
-   vim.keymap.set("n", "<leader>sb", MiniPick.builtin.buffers)
-   vim.keymap.set("n", "<leader>sh", MiniPick.builtin.help)
-   vim.keymap.set("n", "<leader>st", MiniExtra.pickers.diagnostic)
-   vim.keymap.set("n", "<leader>sr", MiniExtra.pickers.registers)
+	require("mini.pick").setup()
+	vim.keymap.set("n", "<leader>f", MiniPick.builtin.files)
+	vim.keymap.set("n", "<leader>ss", MiniPick.builtin.grep_live)
+	vim.keymap.set("n", "<leader>sg", MiniPick.builtin.grep)
+	vim.keymap.set("n", "<leader>sb", MiniPick.builtin.buffers)
+	vim.keymap.set("n", "<leader>sh", MiniPick.builtin.help)
+	vim.keymap.set("n", "<leader>st", MiniExtra.pickers.diagnostic)
+	vim.keymap.set("n", "<leader>sr", MiniExtra.pickers.registers)
 end)
-
-
 
 later(function()
-   require("mini.files").setup()
-   vim.keymap.set("n", "<leader>e", MiniFiles.open)
+	require("mini.files").setup()
+	vim.keymap.set("n", "<leader>e", MiniFiles.open)
 end)
 
-require('mini.indentscope').setup()
+require("mini.indentscope").setup()
 
-require('mini.misc').setup()
+require("mini.misc").setup()
 vim.keymap.set("n", "<leader>z", MiniMisc.zoom)
 
+later(function()
+	add("neovim/nvim-lspconfig")
+	add("stevearc/conform.nvim")
+	require("plugins.lsp")
+end)
+
+require("mini.completion").setup()
+
+require("mini.jump2d").setup()
+
+require("mini.clue").setup({
+	triggers = {
+		-- leader triggers
+		{ mode = "n", keys = "<leader>" },
+		{ mode = "x", keys = "<leader>" },
+
+		-- Built-in completion
+		{ mode = "i", keys = "<C-x>" },
+
+		-- `g` key
+		{ mode = "n", keys = "g" },
+		{ mode = "x", keys = "g" },
+
+		-- Marks
+		{ mode = "n", keys = "'" },
+		{ mode = "n", keys = "`" },
+		{ mode = "x", keys = "'" },
+		{ mode = "x", keys = "`" },
+
+		-- Registers
+		{ mode = "n", keys = '"' },
+		{ mode = "x", keys = '"' },
+		{ mode = "i", keys = "<C-r>" },
+		{ mode = "c", keys = "<C-r>" },
+
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
+
+		-- `z` key
+		{ mode = "n", keys = "z" },
+		{ mode = "x", keys = "z" },
+	},
+})
+
 add({
-   source = 'neovim/nvim-lspconfig',
+	source = "mfussenegger/nvim-dap",
 })
-
-require('plugins.lsp')
-
-require('mini.completion').setup()
-
-require('mini.jump2d').setup()
-
-require('mini.clue').setup({
-   triggers = {
-      -- leader triggers
-      { mode = "n", keys = '<leader>' },
-      { mode = "x", keys = '<leader>' },
-
-      -- Built-in completion
-      { mode = 'i', keys = '<C-x>' },
-
-      -- `g` key
-      { mode = 'n', keys = 'g' },
-      { mode = 'x', keys = 'g' },
-
-      -- Marks
-      { mode = 'n', keys = "'" },
-      { mode = 'n', keys = '`' },
-      { mode = 'x', keys = "'" },
-      { mode = 'x', keys = '`' },
-
-      -- Registers
-      { mode = 'n', keys = '"' },
-      { mode = 'x', keys = '"' },
-      { mode = 'i', keys = '<C-r>' },
-      { mode = 'c', keys = '<C-r>' },
-
-      -- Window commands
-      { mode = 'n', keys = '<C-w>' },
-
-      -- `z` key
-      { mode = 'n', keys = 'z' },
-      { mode = 'x', keys = 'z' },
-   },
-})
-
-add({
-   source = "mfussenegger/nvim-dap",
-})
-require('plugins.dap')
+require("plugins.dap")
 
 -- dependencies ===================
-now(function() add('nvim-tree/nvim-web-devicons') end)
-
-later(function()
-   local ts_spec = {
-      source = "nvim-treesitter/nvim-treesitter",
-   }
-   add({
-      source = 'nvim-treesitter/nvim-treesitter',
-      -- Use 'master' while monitoring updates in 'main'
-      checkout = 'master',
-      monitor = 'main',
-      -- Perform action after every checkout
-      hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-   })
-
-   local ensure_installed = {
-      'c', 'cpp', 'css', 'html', 'javascript', 'json', 'lua', 'markdown', 'markdown_inline', 'python', 'rust', 'toml',
-      'tsx', 'yaml', 'vim', 'vimdoc'
-   }
-   require('nvim-treesitter.configs').setup({
-      ensure_installed = ensure_installed,
-      highlight = { enable = true },
-      incremental_selection = { enable = false },
-      textobjects = { enable = false },
-      indent = { enable = false },
-   })
+now(function()
+	add("nvim-tree/nvim-web-devicons")
 end)
 
 later(function()
-   require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'lua', 'vimdoc' },
-      highlight = { enable = true },
-   })
+	local ts_spec = {
+		source = "nvim-treesitter/nvim-treesitter",
+	}
+	add({
+		source = "nvim-treesitter/nvim-treesitter",
+		-- Use 'master' while monitoring updates in 'main'
+		checkout = "master",
+		monitor = "main",
+		-- Perform action after every checkout
+		hooks = {
+			post_checkout = function()
+				vim.cmd("TSUpdate")
+			end,
+		},
+	})
+
+	local ensure_installed = {
+		"c",
+		"cpp",
+		"css",
+		"html",
+		"javascript",
+		"json",
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"python",
+		"rust",
+		"toml",
+		"tsx",
+		"yaml",
+		"vim",
+		"vimdoc",
+	}
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = ensure_installed,
+		highlight = { enable = true },
+		incremental_selection = { enable = false },
+		textobjects = { enable = false },
+		indent = { enable = false },
+	})
+end)
+
+later(function()
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = { "lua", "vimdoc" },
+		highlight = { enable = true },
+	})
 end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,54 @@
-local lspconfig = require('lspconfig')
-
-; (function(conf)
-   for _, v in ipairs(conf) do
-      if type(v) == "string" then
-         lspconfig[v].setup({})
-      elseif type(v) == "table" then
-         lspconfig[v[1]].setup(v[2])
-      end
-   end
+local lspconfig = require("lspconfig");
+(function(conf)
+	for _, v in ipairs(conf) do
+		if type(v) == "string" then
+			lspconfig[v].setup({})
+		elseif type(v) == "table" then
+			lspconfig[v[1]].setup(v[2])
+		end
+	end
 end)({
-       "ccls",
-       "lua_ls",
-       "tsserver",
-       "zls",
-       "nil_ls"
-    })
+	"ccls",
+	"lua_ls",
+	"tsserver",
+	"zls",
+	"nil_ls",
+})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-   callback = function(args)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = args.buf })
-      vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, { buffer = args.buf })
-      vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { buffer = args.buf })
-      vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { buffer = args.buf })
-      vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, { buffer = args.buf })
-   end,
+local conform = require("conform")
+
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "alejandra" },
+	},
+})
+
+local autoFormatFiletypes = {
+	"nix",
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = args.buf })
+		vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { buffer = args.buf })
+		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = args.buf })
+		vim.keymap.set("n", "<leader>lf", conform.format, { buffer = args.buf })
+		vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { buffer = args.buf })
+	end,
+})
+
+local formatPattern = ""
+for i, v in ipairs(autoFormatFiletypes) do
+	if i ~= 1 then
+		formatPattern = formatPattern .. ","
+	end
+	formatPattern = formatPattern .. "*." .. v
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = formatPattern,
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
