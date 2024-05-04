44af854f5d740323f634bce3ee73841aa0492cff
xunuwu <xunuwu@gmail.com>
2024-05-04 07:34:25 +0200

    minifiles toggle hidden + spawn in open buffer dir, remove jump2d

--- init.lua
+++ init.lua
@@ -52,7 +52,41 @@ end)
 
 later(function()
 	require("mini.files").setup()
-	vim.keymap.set("n", "<leader>e", MiniFiles.open)
+	vim.keymap.set("n", "<leader>r", function()
+		if not MiniFiles.close() then
+			MiniFiles.open()
+		end
+	end)
+	vim.keymap.set("n", "<leader>e", function()
+		if not MiniFiles.close() then
+			MiniFiles.open(vim.fn.expand("%:h"))
+		end
+	end)
+
+	local show_dotfiles = true
+
+	local filter_show = function(fs_entry)
+		return true
+	end
+
+	local filter_hide = function(fs_entry)
+		return not vim.startswith(fs_entry.name, ".")
+	end
+
+	local toggle_dotfiles = function()
+		show_dotfiles = not show_dotfiles
+		local new_filter = show_dotfiles and filter_show or filter_hide
+		MiniFiles.refresh({ content = { filter = new_filter } })
+	end
+
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "MiniFilesBufferCreate",
+		callback = function(args)
+			local buf_id = args.data.buf_id
+			-- Tweak left-hand side of mapping to your liking
+			vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+		end,
+	})
 end)
 
 require("mini.indentscope").setup()
@@ -67,8 +101,6 @@ later(function()
 end)
 
 require("mini.completion").setup()
-
-require("mini.jump2d").setup()
 
 require("mini.clue").setup({
 	triggers = {
